cd7aeddaca5b3aa685deeacd9ff384c383dbe094
vgiang <Giang221295>
2019-07-05 12:43:51 +0700

    first commit

---
+++ src/main/java/com/codegym/cms/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.codegym.cms.repository;
+
+import com.codegym.cms.model.Customer;
+
+import java.util.List;
+
+public interface CustomerRepository {
+    List<Customer> findAll();
+
+    Customer findById(Long id);
+
+    void save(Customer model);
+
+    void remove(Long id);
+}
